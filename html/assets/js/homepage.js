ebb71f1df40dcddf06d1f82771ecf5c7607e7e66
hunghk <hkhung.is@gmail.com>
2016-05-18 13:24:58 +0700

    update menu

--- html/assets/js/homepage.js
+++ html/assets/js/homepage.js
@@ -181,19 +181,22 @@
   }, function() {
     $(this).removeClass('open');
   });
-
-  $(".dropdown-menu > li > a.sub-menu-item").on("click", function(e) {
-    var current = $(this).next();
-    var grandparent = $(this).parent().parent();
+  
+  $(".dropdown-menu > li > a.sub-menu-item").on("click",function(e){
+    var current=$(this).next();
+    var grandparent=$(this).parent().parent();
+    if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+      $(this).toggleClass('right-caret left-caret');
+    grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
     grandparent.find(".sub-menu:visible").not(current).hide();
     current.toggle();
     e.stopPropagation();
   });
-
-  $(".dropdown-menu > li > a:not(.sub-menu-item)").on("click", function() {
-    var root = $(this).closest('.dropdown');
+  $(".dropdown-menu > li > a:not(.sub-menu-item)").on("click",function(){
+    var root=$(this).closest('.dropdown');
+    root.find('.left-caret').toggleClass('right-caret left-caret');
     root.find('.sub-menu:visible').hide();
-  }); 
+  });
   new WOW().init();
 })(jQuery);
 
